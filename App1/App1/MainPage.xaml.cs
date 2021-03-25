b1f393c1af87200ebaace4ad54fc0b6098cce741
dever <dever@PC>
2021-03-25 10:50:39 +0200

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Messaging;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rnd;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSend_Clicked(object sender, EventArgs e)
+        {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                emailMessenger.SendEmail($"{picker.SelectedItem}", $"{EntrySubject}", $"{EntryRecipients}");
+            }
+            ButtonSend.Text = "Отправлено!";
+        }
+
+        private void randombutton_Clicked(object sender, EventArgs e)
+        {
+            picker.Title = "Вы выбрали: " + picker.Items[picker.SelectedIndex];
+            if (picker.SelectedIndex == 0)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 1)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 2)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 3)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 4)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+        }
+    }
+}
